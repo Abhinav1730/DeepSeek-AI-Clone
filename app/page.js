df65a17174407842d285600699b8af435089dc04
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-04-18 18:24:43 +0530

    Creating PrompBox for the users to ask any query and adding functionalities to it.

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import PromptBox from "./components/PromptBox";
 
 export default function Home() {
   const [expand, setExpand] = useState(false);
@@ -13,7 +14,7 @@ export default function Home() {
     <div>
       <div className="flex h-screen">
         {/*---------SideBar--------------- */}
-        <Sidebar expand={expand} setExpand={setExpand}/>
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image
@@ -27,7 +28,7 @@ export default function Home() {
           {message.length === 0 ? (
             <>
               <div className="flex items-center gap-3">
-                <Image alt="" src={assets.logo_icon} className="h-16"/>
+                <Image alt="" src={assets.logo_icon} className="h-16" />
                 <p className="text-2xl font-medium">Hi! , I am DeepSeek</p>
               </div>
               <p className="text-sm mt-2">How can I help you today?</p>
@@ -35,8 +36,10 @@ export default function Home() {
           ) : (
             <div></div>
           )}
-          {/*-------------Prompt Box--------------*/}
-          <p className="text-xs absolute bottom-1 text-gray-500">AI-genrated for reference only</p>
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
+          <p className="text-xs absolute bottom-1 text-gray-500">
+            AI-genrated for reference only
+          </p>
         </div>
       </div>
     </div>
